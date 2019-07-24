cdaf26501db03360ada7b9d5c111457dbf1812b0
Ali Mert Uytun <uytunmert801@gmail.com>
2019-07-24 06:55:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function BoardMember (name, homeState, training) {
   this.training = training;
 }
 
-BoardMember.prototype.sayHi = function () {
+BoardMember.prototype.veto = function () {
   return 'No, I must disagree'
 }
